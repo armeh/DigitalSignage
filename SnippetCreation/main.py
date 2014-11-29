a4a847749c030806e887e6698920febd0cbbf133
armeh <armehendriksz17@gmail.com>
2014-11-29 22:05:18 +0000

    d

--- SnippetCreation/main.py
+++ SnippetCreation/main.py
@@ -21,19 +21,26 @@ class Snippet(polymodel.PolyModel):
     author = ndb.UserProperty()
 	snippet_title = StringProperty(repeated=True)
     snippet_tags = StringProperty(repeated=True)
-	
-class Snippet_Quiz_Question(Snippet)
-	question = ndb.StringProperty(indexed=False)
-	answer = ndb.StringProperty(indexed=False)
 
-class Snippet_Event(Snippet)
+def Snippet_Quiz_Question_Key(snippet_name='Snippet_Quiz_Question'):
+    return ndb.Key('Snippet_Quiz_Question', snippet_name)
+	
+class Snippet_Quiz_Question(Snippet):
+	question = ndb.StringProperty()
+	answer = ndb.StringProperty()
+
+def Snippet_Event_Key(snippet_name='Snippet_Event'):
+    return ndb.Key('Snippet_Event', snippet_name)
+
+class Snippet_Event(Snippet):
 	event_date = ndb.DateProperty()
 	event_time = ndb.TimeProperty()
-	event_content = ndb.StringProperty(indexed=False)
+	event_content = ndb.StringProperty()
 
 class Main(webapp2.RequestHandler):
     def get(self):
-            if users.get_current_user():
+        
+        if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
@@ -50,17 +57,20 @@ class Main(webapp2.RequestHandler):
 
 class Snippet_Insert(webapp2.RequestHandler):
     def post(self):
-        
-				
+        		
 		#set correct properties depending on kind of entry
 		
 		#if form type == q_question then make new that snippet
 		#else if form type == event_snippet then make new event_snippet
 		 
-		if self.request.get('value') == 'q_question'
-			snippet_Qstion = Snippet_Quiz_Question()
-			if users.get_current_user():
-				snippet_Qstion.author = users.get_current_user()
+		#if self.request.get('value') == 'q_question'
+		
+		snippet_name = self.request.get('snippet_name','Snippet_Quiz_Question')
+			
+		snippet_Qstion = Snippet_Quiz_Question(parent=Snippet_Quiz_Question_Key(snippet_name))
+		
+		if users.get_current_user():
+			snippet_Qstion.author = users.get_current_user()
 			
 		snippet_Qstion.snippet_title = self.request.get('snippet_title')
 		snippet_Qstion.snippet_tags = self.request.get('snippet_tags')
@@ -69,20 +79,22 @@ class Snippet_Insert(webapp2.RequestHandler):
 				
 		snippet_Qstion.put()
 		
-		else if self.request.get('value') == 'event_snippet'
-			snippet_Event = Snippet_Event()
-			if users.get_current_user():
-				snippet_Event.author = users.get_current_user()
-			
-		snippet_Event.snippet_title = self.request.get('snippet_title')
-		snippet_Event.snippet_tags = self.request.get_all('snippet_tags')
-		snippet_Event.event_date = self.request.get('event_date')
-		snippet_Event.event_time = self.request.get('event_time')
-		snippet_Event.event_content = self.request.get('event_content')
-			
-		snippet_Event.put()
-
-        query_params = {'guestbook_name': guestbook_name}
+		
+		query_params = {'snippet_name': snippet_name}
         self.redirect('/?' + urllib.urlencode(query_params))
+		
+		#else if self.request.get('value') == 'event_snippet'
+		#	snippet_Event = Snippet_Event()
+			
+		#	if users.get_current_user():
+		#		snippet_Event.author = users.get_current_user()
+			
+		#snippet_Event.snippet_title = self.request.get('snippet_title')
+		#snippet_Event.snippet_tags = self.request.get_all('snippet_tags')
+		#snippet_Event.event_date = self.request.get('event_date')
+		#snippet_Event.event_time = self.request.get('event_time')
+		#snippet_Event.event_content = self.request.get('event_content')
+			
+	    #snippet_Event.put()
 
 app = webapp2.WSGIApplication([('/', Main),('/sign', Snippet),], debug=True)
